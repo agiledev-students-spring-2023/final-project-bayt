8509c76938698a4327845806d9bfd96e0f5d425b
zion-off <ajz317@nyu.edu>
2023-03-02 02:29:38 -0500

    made header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,10 @@
 import './App.css';
-import './css/branding.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Login from './pages/Login';
-import Tasks from './pages/Tasks';
-import Home from './pages/Home';
-
-// TODOS:
-// 1. The index page is login page right now. Should be set to home page once we have user authentication
+import Header from "./header";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <div>
-          <Routes>
-            <Route index path="/" element={<Login />}></Route>
-            <Route index path="/tasks" element={<Tasks />}></Route>
-            <Route index path="/home" element={<Home />}></Route>
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <Header />
     </div>
   );
 }
