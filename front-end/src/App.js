be036fb9a794d901b2fbfef1da17290d8914aa2c
zion-off <ajz317@nyu.edu>
2023-03-04 21:37:35 -0500

    added finances page, need to style it

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
-import Header from "./header";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Finances from "./finances";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from './pages/Login';
+import Finances from './finances';
+
+// TODOS:
+// 1. The index page is login page right now. Should be set to home page once we have user authentication
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Router>
-        <Routes>
-          <Route path="./finances" element={<Finances />} />
-        </Routes>
-      </Router>
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route index path="/" element={<Login />}></Route>
+            <Route index path="/finances" element={<Finances />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
